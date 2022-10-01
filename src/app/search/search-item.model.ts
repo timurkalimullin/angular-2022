f7a5f6f13a953ae72e55f59c3fdc4651c267e186
Timur Kalimullin <timur_kalimullin@list.ru>
2022-10-01 15:59:23 +0500

    add search interfaces

---
+++ src/app/search/search-item.model.ts
@@ -0,0 +1,48 @@
+export interface SearchItem {
+  kind: string;
+  etag: string;
+  id: string;
+  snippet: Snippet;
+  statistics: Statistics;
+}
+
+export interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: Localized;
+  defaultAudioLanguage: string;
+}
+
+export interface Thumbnails {
+  default: ThumbSize;
+  medium: ThumbSize;
+  high: ThumbSize;
+  standard: ThumbSize;
+  maxres: ThumbSize;
+}
+
+export interface ThumbSize {
+  url: string;
+  width: number;
+  height: number;
+}
+
+export interface Localized {
+  title: string;
+  description: string;
+}
+
+export interface Statistics {
+  viewCount: string;
+  likeCount: string;
+  dislikeCount: string;
+  favoriteCount: string;
+  commentCount: string;
+}
