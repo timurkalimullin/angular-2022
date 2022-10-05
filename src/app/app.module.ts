0abfe8e3cecac56093cbf9267d6cb371c904a35d
Timur Kalimullin <timur_kalimullin@list.ru>
2022-10-05 16:45:30 +0500

    add base filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -19,6 +20,7 @@ import { SearchComponent } from './search/search.component';
     MatFormFieldModule,
     MatIconModule,
     MatButtonModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +6,13 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  value: string = '';
+  searchValue: string = '';
 
-  @Output() search = new EventEmitter<string>();
+  filterValue: string = '';
 
-  onChange(val: string) {
-    this.search.emit(val);
+  showFilter = true;
+
+  toggleFilter() {
+    this.showFilter = !this.showFilter;
   }
 }
